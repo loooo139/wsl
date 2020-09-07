815d00ca2bd14cf166c9aac46ea68565e93dc474
lixuefeng <loooo139@gmail.com>
2020-09-07 21:17:59 +0800

    db and redis change for v4

--- crawler.py
+++ crawler.py
@@ -6,7 +6,7 @@
 @Author: li xuefeng
 @Date: 2020-07-25 01:06:38
 
-LastEditTime: 2020-09-01 22:11:52
+LastEditTime: 2020-09-07 21:17:08
 LastEditors: lixf
 @Description: 
 FilePath: \wsl\crawler.py
@@ -94,7 +94,7 @@ mysql = pymysql.connect(host=mysql_url,
                         connect_timeout=200,
                         db='crawler')
 cursor = mysql.cursor()
-sql = 'insert into wsl_news_v3(key_word,start_date,end_date,news_tag,news_title,news_author,news_time,news_summary,news_url,news_index) values("{}","{}","{}","{}","{}","{}","{}","{}","{}","{}")'
+sql = 'insert into wsl_news_v4(key_word,start_date,end_date,news_tag,news_title,news_author,news_time,news_summary,news_url,news_index) values("{}","{}","{}","{}","{}","{}","{}","{}","{}","{}")'
 fail_data = open('./other.csv', 'a', encoding='utf8')
 while True:
     try:
@@ -195,7 +195,7 @@ while True:
                                  author, date, absrtact, news_url, news_index))
                             res.write(single_res + '\n')
                             print(single_res)
-                            if r.sadd('news_author', single_res) == 0:
+                            if r.sadd('news_v4', single_res) == 0:
                                 print('duplicate news')
                                 continue
                             try:
